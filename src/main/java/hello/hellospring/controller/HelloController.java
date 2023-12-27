fd350b532611cd4455a111b9de07728ac0f7237f
seungbeen <seungbin803@gmail.com>
2023-12-27 19:20:44 +0900

    12월 27일 스프링 웹 기초 - API (ResponseBody, JSON, HttpMessageConVerter)

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,5 +19,38 @@ public class HelloController {
     public String helloMvc(@RequestParam(name = "name", required = true) String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        // ResponseBody : http 통신 프로토콜에 head와 body가 있는데
+        // 응답 body 안에 내용을 넣어주겠다는 의미
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam ("name") String name) {
+        // 객체 생성
+        Hello hello = new Hello();
+        // 객체를 넘김
+        hello.setName(name);
+        // json으로 반환됨
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        // 꺼낼 때는 getName
+        public String getName() {
+            return name;
+        }
+
+        // 넣을 때는 setName
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
